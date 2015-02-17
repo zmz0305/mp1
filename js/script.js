162c416cb80f30e2fff90c0e1c7abe2e5f813778
zmz305 <zmz305@gmail.com>
2015-02-17 15:58:13 -0600

    2

--- js/script.js
+++ js/script.js
@@ -43,16 +43,16 @@ $(
            $('header').removeClass('small').addClass('large');
        }
     }),
-    function(){
-        // Set the interval to be 5 seconds
-        console.log("adsfadf");
-        var t = setInterval(function(){
-            $("#carousel ul").animate({marginLeft:-480},1000,function(){
-                $(this).find("li:last").after($(this).find("li:first"));
-                $(this).css({marginLeft:0});
-            })
-        },5000);
-    }
+    //function(){
+    //    // Set the interval to be 5 seconds
+    //    console.log("adsfadf");
+    //    var t = setInterval(function(){
+    //        $("#carousel ul").animate({marginLeft:-480},1000,function(){
+    //            $(this).find("li:last").after($(this).find("li:first"));
+    //            $(this).css({marginLeft:0});
+    //        })
+    //    },5000);
+    //}
 
 
 
@@ -62,53 +62,29 @@ $(
 
 
 $(function() {
-    //rotation speed and timer
     var speed = 5000;
     var run = setInterval('rotate()', speed);
-
-    //grab the width and calculate left value
     var item_width = $('#slides li').width();
     var left_value = item_width * (1);
 
-    //move the last item before first item, just in case user click prev button
     $('#slides li:first').before($('#slides li:last'));
-
-    //set the default item to the correct position
     $('#slides ul').css({'left' : left_value});
-    //if user clicked on prev button
     $('#prev').click(function() {
-        //get the right position
         var left_indent = parseInt($('#slides ul').css('left')) + item_width;
-        //slide the item
         $('#slides ul').animate({'left' : left_indent}, 200,function(){
-            //move the last item and put it as first item
             $('#slides li:first').before($('#slides li:last'));
-            //set the default item to correct position
             $('#slides ul').css({'left' : left_value});
-
         });
-        //cancel the link behavior
         return false;
 
     });
-    //if user clicked on next button
     $('#next').click(function() {
-
-        //get the right position
         var left_indent = parseInt($('#slides ul').css('left')) - item_width;
-
-        //slide the item
         $('#slides ul').animate({'left' : left_indent}, 200, function () {
-
-            //move the first item and put it as last item
             $('#slides li:last').after($('#slides li:first'));
-
-            //set the default item to correct position
             $('#slides ul').css({'left' : left_value});
-
         });
 
-        //cancel the link behavior
         return false;
 
     });
